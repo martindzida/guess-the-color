91e93d523f0c1832e92ee8924b29c8180a711717
martindzida <martin.d14@email.cz>
2022-10-08 23:12:26 +0200

    Guessing and streak

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,14 @@ import Button from './Button';
 function App() {
   const [curColors, setCurColors] = useState<string[]>([]);
   const [correctColor, setCorrectColor] = useState<string>();
-  const [isCorrect, setIsCorrect] = useState<boolean | null>();
+  const [isCorrect, setIsCorrect] = useState<boolean | null>(null);
+  const [goodGuess, setGoodGuess] = useState(false);
+  const [streak, setStreak] = useState(0);
 
   useEffect(() => {
     setCurColors(genColors());
-  }, []);
+    setGoodGuess(false);
+  }, [goodGuess]);
 
   const genColors = () => {
     const hex = '0123456789ABCDEF';
@@ -26,16 +29,30 @@ function App() {
 
   const handlePick = (color: string) => {
     setIsCorrect(correctColor === color);
+    if (correctColor === color) {
+      setGoodGuess(true);
+      setStreak(streak + 1);
+    } else {
+      setStreak(0);
+    }
   };
 
   return (
     <div className='w-screen h-screen flex flex-col justify-center items-center bg-amber-400'>
-      <h1 className='font-extrabold text-5xl p-10 text-slate-700'>Guess the color</h1>
+      <h1 className='font-extrabold text-6xl p-10 text-slate-700'>Guess the color</h1>
+      <div className='flex items-center text-2xl font-bold text-slate-700 p-8'>
+        <span>Current streak: </span>
+        <span className='text-5xl text-orange-600 mx-4'>{streak}</span>
+      </div>
       <div className='w-60 h-60' style={{backgroundColor: correctColor}}></div>
       <div className='flex justify-center p-10'>
         {curColors.map((c: string) => (
           <Button key={c} color={c} handler={handlePick} />
         ))}
+      </div>
+      <div className={`${isCorrect ? 'text-green-500' : 'text-rose-500'} text-2xl font-bold`}>
+        {isCorrect !== null && isCorrect && 'Correct!'}
+        {isCorrect !== null && !isCorrect && 'Wrong :c'}
       </div>
     </div>
   );
